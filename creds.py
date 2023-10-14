35b3527db846c3c01e0b4050cacf51b9efed87fa
Christine Simon <41668253+N1ghtstsorm3r@users.noreply.github.com>
2023-10-14 15:38:42 -0500

    Create creds.py
    All prior accessed information

---
+++ creds.py
@@ -0,0 +1,10 @@
+""" Christine Simon: 1840920
+"""
+class creds:
+    connectionstring = 'cis3368christine.cp8qyjtysdfh.us-east-1.rds.amazonaws.com'
+    username = 'Chrissy24'
+    passwd = 'Chrissy2766!'
+    dataBase = 'CIS3368christine'
+    #You dont have to rewrite it agian, it will also protect your information
+#All creds information is borrowed from previous homeworks and test
+    
